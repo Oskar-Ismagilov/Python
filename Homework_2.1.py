dfbcc483cfdc8b810853259ed96934681b19ce82
Oskar-Ismagilov <oscar-is@yandex.ru>
2021-09-29 19:27:30 +0300

    Оптимизирован алгоритм задания №1

--- Homework_2.1.py
+++ Homework_2.1.py
@@ -1,5 +1,3 @@
 new_list = [1, "symbol", 3.576, True]
-print(f"Тип данных первого символа - {type(new_list[0])}")
-print(f"Тип данных второго символа - {type(new_list[1])}")
-print(f"Тип данных третьего символа - {type(new_list[2])}")
-print(f"Тип данных четвертого символа - {type(new_list[3])}")
+for i in range(len(new_list)):
+    print(f"Тип данных символа №{i + 1} - {type(new_list[i])}")
